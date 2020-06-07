ca84f88fd7a5df16ff96d39b22bf26429ce17343
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-06-07 19:53:40 +0200

    création logique metier login + verification et cryptage mdp avec bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -17,4 +17,25 @@ exports.createUser = (req, res, next) => {
         .catch(error => res.status(500).json({error}))
 };
 
-exports.loginUser = (req, res, next) => {};
+exports.loginUser = (req, res, next) => {
+    User.findOne({ email: req.body.email})
+        .then(user => {
+            if(!user){
+                return res.status(401).json({ error : "Utilisateur non trouvé !"})
+            }else{
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid =>{
+                        if(!valid){
+                            return res.status(401).json({error: "Mot de passe incorrect !"})
+                        }else{
+                            res.status(200).json({
+                                userId: user._id,
+                                token: "ok"
+                            })
+                        }
+                    })
+                    .catch(error => res.status(500).json({error}))
+            }
+        })
+        .catch(error => res.status(500).json({error}))
+};
