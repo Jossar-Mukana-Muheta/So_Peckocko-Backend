3c132185bc935bf5b99b6f0377836ca0802a8e0d
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-06-07 21:07:33 +0200

    l’authentification est renforcée sur les routes requises

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../middleware/auth')
 
 const sauceController = require("../controllers/sauce");
 
 //---------------- Routes Get
-router.get("/", sauceController.getAll);
+router.get("/",auth, sauceController.getAll);
 router.get("/:id", sauceController.getOne);
 
 //--------------- Routes Post
 
-router.post("/", sauceController.creatOne);
+router.post("/",auth, sauceController.creatOne);
 
 router.post("/:id/like", sauceController.likeOne);
 
 //----------------- Routes Put
 
-router.put("/:id", sauceController.modifieOne);
+router.put("/:id",auth, sauceController.modifieOne);
 
 //----------------- Routes Delet
 
-router.delete("/:id", sauceController.deleteone);
+router.delete("/:id",auth, sauceController.deleteone);
 
 module.exports = router;
