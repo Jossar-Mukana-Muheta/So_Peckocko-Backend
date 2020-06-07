a5c5e7898fc8f197ca38a28c3708f16b50615d4b
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-06-07 18:32:59 +0200

    implémentation createuser ok

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,20 @@
 const User = require("../models/user");
+const bcrypt = require('bcrypt')
 
-//-------------- User
 
-exports.createUser = (req, res, next) => {};
+
+exports.createUser = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email : req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(()=>res.status(200).json({message:"Utilisater crée !"}))
+                .catch(error => res.status(500).json({error}))
+        })
+        .catch(error => res.status(500).json({error}))
+};
 
 exports.loginUser = (req, res, next) => {};
