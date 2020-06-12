425a092859f104867a2c467b08240d4a1200ff2c
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-06-12 22:17:03 +0200

    creer une sauce ok , recupérer sauce ok

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,25 +1,45 @@
 const Sauce = require("../models/sauce");
 
 exports.getAll = (req, res, next) => {
-    Sauce.find()
-        .then(Sauce => res.status(200).json({Sauce}))
-        .catch(error => res.status(400).json({error}))
+  Sauce.find()
+    .then( sauce => res.status(200).json(sauce)
+    )
+    .catch(error => res.status(400).json({ error }));
+    
 };
 
 exports.getOne = (req, res, next) => {
-    Sauce.findOne({_id: req.params.id})
-        .then(sauce => res.status(200).json(sauce))
-        .catch(res.status(400).json({error}))
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(400).json({ error }));
+    
 };
 
 exports.creatOne = (req, res, next) => {
-    const sauce = new Sauce({
-        ...req.body
-    })
-    user.save()
-        .then(sauce => res.status(201).json({message : res.sauce}))
-        .catch(res.status(400).json({error}))
+  const sauceObjet = JSON.parse(req.body.sauce);
+  // Suppression de l'id car generé par defaut par la BDD
+  delete sauceObjet._id;
+  const sauce = new Sauce({
+    ...sauceObjet,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: []
+  });
+console.log(sauceObjet)
+console.log(sauce)
+  sauce
+    .save()
+    .then(
+      res.status(201).json({ message : "sauce enregistré" })
+        
 
+    
+    )
+    .catch(res.status(401).json({ error: "ca passe pas" }));
 };
 
 exports.modifieOne = (req, res, next) => {};
